532f3c82d7dca97c760ab7554f96bb7bcd7a78a6
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2021-03-02 14:42:47 +0100

    Update Trilepton_plotter.py

--- Trilepton_plotter.py
+++ Trilepton_plotter.py
@@ -1,33 +1,42 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.ticker as ticker
-import sys
 
 
-read_DF = pd.read_hdf("Trilepton_ML.h5", key="DF_flat")
-print(read_DF.info())
-#size = len(read_DF)
+read_DF = pd.read_hdf("Trilepton_ML.h5", key="DF_flat3")
+#print(read_DF.info())
 
-### DataFrames and plotting: ###
+
+### Functions for plotting various variables in the dataframe: ###
 def plotter(var):
-    df = read_DF[["lep1_"+var, "lep2_"+var, "lep3_"+var, "lep4_"+var]]
+    """Plots chosen variable property for all the leptons in one plot."""
+    try:
+        df = read_DF[["lep1_"+var, "lep2_"+var, "lep3_"+var, "lep4_"+var]]
 
-    df.plot.hist(stacked=False, histtype="step", bins=100, log=True)
-    plt.title("Plot of "+var+" for each lepton")
-    plt.show()
+        df.plot.hist(stacked=False, histtype="step", bins=100, log=True)
+        plt.title("Plot of "+var+" for each lepton")
+        plt.show()
+    except:
+        print("Variable is not defined in dataframe! Choose another:")
+        print(read_DF.columns)
+
 
 def sub_plotter(var):
+    """Plots a set of chosen variables which are connected to two leptons at a time, where each subplot shows the connection of the i'th lepton to the other leptons."""
     fig, axes = plt.subplots(4, 1, figsize=(12,8), constrained_layout=True)
     ax0, ax1, ax2, ax3 = axes.flatten()
 
     if var == "mll":
-        fig.suptitle("Invariant mass between lepton -i- and -j- in mll_ij", fontsize=16)
+        fig.suptitle("Invariant mass of lepton -i- and -j- in mll_ij", fontsize=16)
     elif var == "dphi":
-        fig.suptitle("Azimuthal angle between lepton -i- and -j- in mll_ij", fontsize=16)
+        fig.suptitle("Azimuthal angle between lepton -i- and -j- in dPhi_ij", fontsize=16)
     elif var == "dR":
-        fig.suptitle("Angular distance between lepton -i- and -j- in mll_ij", fontsize=16)
+        fig.suptitle("Angular distance between lepton -i- and -j- in dR_ij", fontsize=16)
     else:
+        print("Choose a variable in the dataframe:")
+        print(read_DF.columns)
         sys.exit()
 
     df_1 = read_DF[[var+"_12", var+"_13", var+"_14"]]
@@ -45,11 +54,11 @@ def sub_plotter(var):
 
 """ 
 Available variables to plot:
-    -plotter: pt, phi, eta, theta, px, py, pz, E, vtx, pid
-    -sub_plotter: mll, dphi, dR
+    -plotter (10): pt, phi, eta, theta, px, py, pz, E, vtx, pid
+    -sub_plotter (3): mll, dphi, dR
 """
 
 plotter("pt")
-sub_plotter("mll")
+#sub_plotter("mll")
 #sub_plotter("dphi")
 #sub_plotter("dR")
